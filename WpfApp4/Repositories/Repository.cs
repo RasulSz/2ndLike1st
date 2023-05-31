23f1eb7eacaff0ff98a8719f01193e9055696640
XSbzi_Uh51 <XSbzi_Uh51@STHQ0127-06>
2023-05-31 13:38:33 +0400

    Add project files.

---
+++ WpfApp4/Repositories/Repository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp4.Models;
+
+namespace WpfApp4.Repositories
+{
+    public class Repository
+    {
+        SqlConnection conn;
+        string cs = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        DataSet set = new DataSet();
+        public Repository()
+        {
+            conn = new SqlConnection();
+
+            using (conn = new SqlConnection())
+            {
+                var da = new SqlDataAdapter();
+                conn.ConnectionString = cs;
+                conn.Open();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM Authors", conn);
+
+                da.SelectCommand = command;
+                da.Fill(set, "AuthorsSet");
+            }
+        }
+
+        public DataSet GetAll()
+        {
+            return set;
+        }
+
+        public void Insert(int id, string firstName, string lastName)
+        {
+            using (conn = new SqlConnection())
+            {
+                var command = new SqlCommand("INSERT INTO Authors(Id,FirstName,LastName) VALUES(@id,@firstName,@lastName)", conn);
+                conn.ConnectionString = cs;
+                conn.Open();
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    SqlDbType = SqlDbType.Int,
+                    ParameterName = "@id",
+                    Value = id
+                });
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    SqlDbType = SqlDbType.NVarChar,
+                    ParameterName = "@firstName",
+                    Value = firstName
+                });
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    SqlDbType = SqlDbType.NVarChar,
+                    ParameterName = "@lastName",
+                    Value = lastName
+                });
+
+                var da = new SqlDataAdapter();
+                da.InsertCommand = command;
+                da.InsertCommand.ExecuteNonQuery();
+                da.Update(set, "AuthorsSet");
+                set.Clear();
+
+                da = new SqlDataAdapter("SELECT * FROM Authors", conn);
+
+                da.Fill(set, "AuthorsSet");
+
+
+            }
+        }
+    }
+}
---
+++ WpfApp4/ViewModels/MainWindowViewModels.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using WpfApp4.Commands;
+using System.Collections.ObjectModel;
+using WpfApp4.Models;
+using WpfApp4.Repositories;
+
+namespace WpfApp4.ViewModels
+{
+    public class MainWindowViewModels : BaseViewModel
+    {
+
+        private DataSet authorSet;
+
+        public DataSet AuthorSet
+        {
+            get { return authorSet; }
+            set { authorSet = value; OnPropertyChanged(); }
+        }
+
+        public Repository Authorss { get; set; }
+        public RelayCommand InsertCommand { get; set; }
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged(); }
+        }
+
+        private string firstname;
+
+        public string FirstName
+        {
+            get { return firstname; }
+            set { firstname = value; OnPropertyChanged(); }
+        }
+
+        private string lastname;
+
+        public string LastName
+        {
+            get { return lastname; }
+            set { lastname = value; OnPropertyChanged(); }
+        }
+
+        private int index;
+
+        public int Index
+        {
+            get { return index; }
+            set { index = value; OnPropertyChanged(); }
+        }
+
+        public RelayCommand SelectionChanged { get; set; }
+
+
+        public MainWindowViewModels()
+        {
+
+            Authorss = new Repository();
+            AuthorSet = Authorss.GetAll();
+
+
+            InsertCommand = new RelayCommand((obj) =>
+            {
+                Authorss.Insert(Id, FirstName, LastName);
+            });
+        }
+    }
+}
